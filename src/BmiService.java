cb6a25ec938aa20ab34fa7a3df7fd3c1dc1ebfa0
Kaminsky <kamwlad2010@gmail.com>
2023-01-23 18:01:01 +0400

    редактирование кода

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,9 +1,9 @@
 public class BmiService {
 
-    public double calculate(double parameter_1, double parameter_2) {
-        double fraction = parameter_1 / 100;
+    public double calculate(double height, double weight) {
+        double fraction = height / 100;
         double square = (fraction * fraction);
-        double index = parameter_2 / square;
+        double index = weight / square;
 
         return index;
     }
